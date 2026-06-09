79f2a2c0933512f84e7c0f75d892cf4c94e54d96
김근호 <insertvalue@naver.com>
2026-06-09 10:50:54 +0900

    회원정보 수정

--- src/main/java/com/tenco/csr_blog_v1/user/UserService.java
+++ src/main/java/com/tenco/csr_blog_v1/user/UserService.java
@@ -3,6 +3,7 @@ package com.tenco.csr_blog_v1.user;
 import com.tenco.csr_blog_v1.auth.AuthRequest;
 import com.tenco.csr_blog_v1.auth.AuthResponse;
 import com.tenco.csr_blog_v1.core.handler.errors.BadRequestException;
+import com.tenco.csr_blog_v1.core.handler.errors.ForbiddenException;
 import com.tenco.csr_blog_v1.core.handler.errors.NotFoundException;
 import com.tenco.csr_blog_v1.core.handler.errors.UnauthorizedException;
 import com.tenco.csr_blog_v1.core.util.JwtUtil;
@@ -44,6 +45,27 @@ public class UserService {
     public AuthResponse.AvailableDTO 유저네임중복체크(String username) {
        boolean isAvailable = !userRepository.existsByUsername(username);
        return new AuthResponse.AvailableDTO(isAvailable);
+    }
+
+
+    public AuthResponse.DTO 회원조회(Integer userId, Integer sessionUserId) {
+        if(!userId.equals(sessionUserId)) {
+            throw new ForbiddenException("조회 권한이 없습니다");
+        }
+
+        User findUser = userRepository.findById(sessionUserId).orElseThrow(
+                () -> new NotFoundException("회원 정보를 찾을 수 없습니다"));
+        return new AuthResponse.DTO(findUser);
+    }
+
+    @Transactional
+    public AuthResponse.DTO 회원수정(UserRequest.UpdateDTO requestDTO, Integer sessionUserId) {
+        User findUser = userRepository.findById(sessionUserId)
+                .orElseThrow(() -> new NotFoundException("회원을 찾을 수 없습니다"));
+        // 사전 기반 지식 : 사용자 입력 1234 ---> 암호화 처리 해주어야 한다.
+        String encPassword = bCryptPasswordEncoder.encode(requestDTO.password());
+        findUser.update(requestDTO.email(), encPassword); // 더티 체킹
+        return new AuthResponse.DTO(findUser);
     }
 
 }
